2ebf67df19628dea223fc642f5ef31eff5c11c8a
pennycuda <141153875+pennycuda@users.noreply.github.com>
2026-07-09 17:06:12 -0400

    get rid of shinycell dir

--- upload_to_vm.py
+++ upload_to_vm.py
@@ -12,7 +12,7 @@ def get_uuid(data_product_metadata):
     return uuid
 
 
-def upload_to_vm(umap_png, metadata_json, shiny_cell_dir, uuid):
+def upload_to_vm(umap_png, metadata_json, uuid):
     os.system(
         f"scp {umap_png} /opt/repositories/vm024-dev/data-products-ui/pipeline_outputs/{uuid}.png"
     )
@@ -23,7 +23,7 @@ def upload_to_vm(umap_png, metadata_json, shiny_cell_dir, uuid):
 
 def main(umap_png, metadata_json, shiny_cell_dir):
     uuid = get_uuid(metadata_json)
-    upload_to_vm(umap_png, metadata_json, shiny_cell_dir, uuid)
+    upload_to_vm(umap_png, metadata_json, uuid)
 
 
 if __name__ == "__main__":
@@ -32,4 +32,4 @@ if __name__ == "__main__":
     p.add_argument("data_product_metadata", type=Path)
     args = p.parse_args()
 
-    main(args.umap_png, args.data_product_metadata, args.shiny_cell_dir)
+    main(args.umap_png, args.data_product_metadata)
